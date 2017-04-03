aec3d486ccca0c2deba7dc7fbe1a1dba993dd330
dsgfg <dsgfg@gmail.com>
2017-04-03 21:51:31 +0300

    "Magic 5.1"

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -3,15 +3,22 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by trm_cp on 3/18/17.
  */
 public class MainApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         ApiContextInitializer.init();
         TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        ServerSocket ss = new ServerSocket(2234);
+        Socket accept = ss.accept();
 
         try {
             botsApi.registerBot(new KotletkoBot());
