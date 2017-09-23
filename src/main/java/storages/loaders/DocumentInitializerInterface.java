49f28a7ee166176bb9c5784fc963344a3b4b033f
borys <borys.490@gmail.com>
2017-09-23 19:53:16 +0300

    Add base interface for document initializer-class

---
+++ src/main/java/storages/loaders/DocumentInitializerInterface.java
@@ -0,0 +1,8 @@
+package storages.loaders;
+
+import documents.DocumentInterface;
+
+public interface DocumentInitializerInterface {
+
+    public void initializeDocument (DocumentInterface document, int numberOfDocument);
+}
